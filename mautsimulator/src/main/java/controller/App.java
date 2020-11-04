b66ff520c3ca423a3edd29b83624f01eccc387f1
Lehmsen <Marcel-Lehmann2000@web.de>
2020-11-04 13:26:35 +0100

    1.3 marcel

---
+++ mautsimulator/src/main/java/controller/App.java
@@ -0,0 +1,13 @@
+package controller;
+
+import controller.TransitGenerator;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Automatisch generierter Methodenstub
+		TransitGenerator g = new TransitGenerator();
+		g.generateTransit();
+	}
+
+}
--- mautsimulator/src/main/java/controller/Generator.java
+++ mautsimulator/src/main/java/controller/Generator.java
@@ -12,11 +12,21 @@ public class Generator {
 	ArrayList<Vehicle> vehicleList = new ArrayList<Vehicle>();
 
 
-	public void generateVehicle() {
+	public ArrayList<Vehicle> generateVehicle() {
 		
 			User user = new User("0001", "Mustermann", "Max", "Musterstraße", "02826", "40", "Musterstadt", "0123456789");
 			Vehicle vehicle = new Vehicle(Origin.DE, "DD-KM-500", user);
 			vehicleList.add(vehicle);
+			
+			User user2 = new User("0001", "Mustermann", "Max", "Musterstraße", "02826", "40", "Musterstadt", "0123456789");
+			Vehicle vehicle2 = new Vehicle(Origin.DE, "DD-KM-505", user2);
+			vehicleList.add(vehicle2);
+			
+			User user3 = new User("0001", "Mustermann", "Max", "Musterstraße", "02826", "40", "Musterstadt", "0123456789");
+			Vehicle vehicle3 = new Vehicle(Origin.DE, "DD-KM-510", user3);
+			vehicleList.add(vehicle3);
+			
+			return vehicleList;
 	}
 	
 
