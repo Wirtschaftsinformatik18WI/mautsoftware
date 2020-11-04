050549c83f4f2e3518ef986d1fc36b9c29b2a491
Florian Jeremias <florian.jeremias@cideon.com>
2020-11-04 11:13:34 +0100

    1.2

---
+++ mautsimulator/src/main/java/model/Origin.java
@@ -0,0 +1,7 @@
+package model;
+
+public enum Origin {
+	
+	DE, F, PL, CZ, GB, I, LT, L, CH
+	
+}
---
+++ mautsimulator/src/main/java/model/Position.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.util.UUID;
+
+public class Position {
+	
+	private UUID positionID;
+	private String description;
+	
+	public Position(UUID positionID, String description) {
+		this.positionID = positionID;
+		this.description = description;
+	}
+
+	public UUID getPositionID() {
+		return positionID;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+}
